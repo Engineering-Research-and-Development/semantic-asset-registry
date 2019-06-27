8d97aa5fffaf1da914bec45e67350f60a42d1793
nsvulcan <antonio.caruso78@gmail.com>
2019-06-27 11:49:19 +0200

    #Bug concurrency
    Resolved concurrency issue

--- A4bluePelletReasoner/src/it/eng/cam/rest/A4blueSPARQLUpdate.java
+++ A4bluePelletReasoner/src/it/eng/cam/rest/A4blueSPARQLUpdate.java
@@ -1,52 +1,62 @@
 package it.eng.cam.rest;
 
-import it.eng.cam.pellet.sparql.A4blueJenaSPARQLUpdateFactory;
-import it.eng.cam.rest.exception.CAMServiceWebException;
-import it.eng.cam.rest.json.PelletResonerJSON;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.glassfish.jersey.server.ManagedAsync;
+
+import it.eng.cam.pellet.sparql.A4blueJenaSPARQLUpdateFactory;
+import it.eng.cam.rest.exception.CAMServiceWebException;
+import it.eng.cam.rest.json.PelletResonerJSON;
 
 @Path("/")
 public class A4blueSPARQLUpdate {
 
 	/** local logger for this class **/
 	private static Log log = LogFactory.getLog(A4blueSPARQLUpdate.class);
-
+	
 	@POST
 	@Path("/SPARQLInferenceUpdate")
+	@ManagedAsync
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response SPARQLInferenceQuery(PelletResonerJSON json) {
+	public void SPARQLInferenceQuery(PelletResonerJSON json, @Suspended final AsyncResponse asyncResponse) {
 		// check if all form parameters are provided
 		if (json.getDocumentURI() == null || json.getDocumentURI().isEmpty())
-			return Response.status(400).entity("Invalid data input").build();
+			asyncResponse.resume(Response.status(400).entity("Invalid data input").build());
 		if (json.getSparqlQuery() == null || json.getSparqlQuery().isEmpty())
-			return Response.status(400).entity("Invalid data input").build();
-		
-		A4blueJenaSPARQLUpdateFactory instance = 
-				new A4blueJenaSPARQLUpdateFactory();
-		
-		synchronized (instance) {
+			asyncResponse.resume(Response.status(400).entity("Invalid data input").build());
+
+		synchronized (A4blueSPARQLUpdate.class) {
+			log.info("synchronized block");
+	 		A4blueJenaSPARQLUpdateFactory instance = 
+					new A4blueJenaSPARQLUpdateFactory();
+			
 			try {
 				String rdfDocument = instance.executeInferenceUpdate(
 						json.getDocumentURI(), json.getSparqlQuery());
-				if (!instance.putRDFData(json.getDocumentURI(), rdfDocument))
-					throw new CAMServiceWebException("Unable to execute SPARQL inference Update");
+				if (rdfDocument != null) {
+					if (!instance.putRDFData(json.getDocumentURI(), rdfDocument)) {
+						log.error("Unable to execute SPARQL inference Update. Problem with repository");
+						throw new CAMServiceWebException("Unable to execute SPARQL inference Update. Problem with repository");
+					}
+				}
 			} catch (Exception e) {
 				log.error("Unable to execute SPARQL inference Update. Problem with repository", e);
 				throw new CAMServiceWebException(e.getMessage());
 			}
 		}
 		
-		return Response.ok("SPARQL Operation Performed", MediaType.APPLICATION_JSON).build();
+		asyncResponse.resume(Response.ok("SPARQL Operation Performed", MediaType.APPLICATION_JSON).build());		
+		
 	}
 
 }
